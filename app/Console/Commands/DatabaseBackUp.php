e5f3565e3fdddab64b8edd1c72273570871d5eac
Juan Perugachi <juan.perugachi@gruporadical.com>
2021-12-08 11:30:26 -0500

    fix

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -45,17 +45,14 @@ class DatabaseBackUp extends Command
         $command = "".env('DUMP_PATH')." --user=" . env('DB_USERNAME') . " --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  > " . storage_path() . "/app/backup/" . $filename;
         $returnVar = null;
         $output = null;
+
         exec($command, $output, $returnVar);
-        $zipFileName=$fileName;
-        $zipPassword='jk1609PV*';
-        $commandZip ='zip -jrq -P '.$zipPassword.' '.$zipFileName.'.zip '.$fileName;
-        exec($commandZip,$output,$returnVar);
         //dd($output);
 
             $details=[
             'title' => 'Ejecución automática de backup diario',
             'body' => 'Estimad@ el software Registro de actividades ha generado el backup diario automático correspondiente al '.Carbon::now()->format('Y-m-d'),
-            'file' =>  $zipFileName.'.zip'
+            'file' =>  $filename
         ];
         Mail::to('juan.perugachi@gruporadical.com')
                     //->cc(['paul.canchignia@gruporadical.com','xavier.montoya@gruporadical.com'])
