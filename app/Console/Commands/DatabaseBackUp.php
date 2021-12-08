fe5ef878bb111d923bba727b42d7fa6f921fc231
Juan Perugachi <juan.perugachi@gruporadical.com>
2021-12-08 11:27:42 -0500

    send mail

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use App\Mail\BackupEmail;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
 
 class DatabaseBackUp extends Command
 {
@@ -38,11 +40,30 @@ class DatabaseBackUp extends Command
      */
     public function handle()
     {
+
         $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".sql";
         $command = "".env('DUMP_PATH')." --user=" . env('DB_USERNAME') . " --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  > " . storage_path() . "/app/backup/" . $filename;
         $returnVar = null;
         $output = null;
         exec($command, $output, $returnVar);
+        $zipFileName=$fileName;
+        $zipPassword='jk1609PV*';
+        $commandZip ='zip -jrq -P '.$zipPassword.' '.$zipFileName.'.zip '.$fileName;
+        exec($commandZip,$output,$returnVar);
+        //dd($output);
+
+            $details=[
+            'title' => 'Ejecución automática de backup diario',
+            'body' => 'Estimad@ el software Registro de actividades ha generado el backup diario automático correspondiente al '.Carbon::now()->format('Y-m-d'),
+            'file' =>  $zipFileName.'.zip'
+        ];
+        Mail::to('juan.perugachi@gruporadical.com')
+                    //->cc(['paul.canchignia@gruporadical.com','xavier.montoya@gruporadical.com'])
+                    ->send(new BackupEmail($details));
+
+
+
+
 
 
     }
